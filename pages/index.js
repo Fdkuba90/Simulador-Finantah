2498f8edbaf34e0ef91daf1a8c222e6b153fbaaa
Fdkuba90 <picodkuba@hotmail.com>
2025-08-07 10:23:30 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,13 @@
 import Head from 'next/head';
-import Simulator from '../components/Simulator';
+import Simulador from '../components/Simulador';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white flex flex-col items-center justify-start p-4">
+    <div>
       <Head>
         <title>Simulador de Utilidad - FINANTAH</title>
       </Head>
-
-      {/* Logo más pequeño y proporcional */}
-      <img
-        src="/finantah-logo.png"
-        alt="Logo FINANTAH"
-        style={{ width: '160px', height: 'auto', marginTop: '20px' }}
-      />
-
-      <h1 className="text-3xl font-bold text-center mb-8 mt-4">
-        Simulador de Utilidad - FINANTAH
-      </h1>
-
-      <Simulator />
+      <Simulador />
     </div>
   );
 }
